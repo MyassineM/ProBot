79c411ed7a21bdb7b78a4bd31f55ba4c8681c408
streaming-yassine <44115214+MyassineM@users.noreply.github.com>
2018-11-29 19:28:49 +0100

    Update probot.js

--- probot.js
+++ probot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
 const Canvas = require("canvas"); //npm i canvas
-const prefix = "YOUR Prefix Here" // برفكس
+const prefix = "+" // برفكس
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 
@@ -68,7 +68,7 @@ client.on("message", message => {
 
 
 client.on('message', message => {
-    var prefix = "#";
+    var prefix = "+";
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
  
